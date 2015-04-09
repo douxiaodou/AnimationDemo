c5dc33551816a59d8eb5ca7526fc86111808a168
daoneng <daoneng@daonengdeMac-mini.local>
2015-04-09 13:17:18 +0800

    Initial Commit

---
+++ AnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemo
+//
+//  Created by daoneng on 15/4/9.
+//  Copyright (c) 2015年 dawningsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationDemo/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  AnimationDemo
+//
+//  Created by daoneng on 15/4/9.
+//  Copyright (c) 2015年 dawningsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
